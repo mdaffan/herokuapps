f210fee51a77717568b6fed52023484b656a0c88
Md Affan <mohammed.affan4@gmail.com>
2018-07-13 00:12:12 +0530

    after adding mlab

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.use(function(req,res,next){
 app.use("/", indexRoutes)
 app.use("/locations",locationRoute)
 app.use("/locations/:id/comments",commentRoute)
-mongoose.connect("mongodb://localhost:27017/loc_1",{ useNewUrlParser: true });    
+mongoose.connect("mongodb://locations:locations123@ds235411.mlab.com:35411/myapp2",{ useNewUrlParser: true });    
 app.set("view engine", "ejs");    
 app.listen(3000,function (req,res) {
     console.log("Server Exploded")
